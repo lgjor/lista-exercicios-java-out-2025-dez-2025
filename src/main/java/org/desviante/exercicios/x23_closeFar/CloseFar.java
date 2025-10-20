b227484806da51876e3b77380ed60386833433f9
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-10-20 15:58:48 -0300

    Adicionado exercício 23 - comparar valores absolutos

--- src/main/java/org/desviante/exercicios/x23_closeFar/CloseFar.java
+++ src/main/java/org/desviante/exercicios/x23_closeFar/CloseFar.java
@@ -3,6 +3,18 @@ package org.desviante.exercicios.x23_closeFar;
 public class CloseFar {
 
 	public boolean closeFar(int a, int b, int c) {
+
+		// Verifico a distância de a e b
+		if (Math.abs(a - b) <=1){
+			if (Math.abs(b - c) >= 2 && Math.abs(a - c) >= 2) {
+				return true;
+			}
+		// Verifico a distância de a e c
+		} else if (Math.abs(a - c)<=1){
+			if (Math.abs(b - c) >= 2 && Math.abs(a - b) >= 2) {
+				return true;
+			}
+		}		
 		return false;
 	}
 }
