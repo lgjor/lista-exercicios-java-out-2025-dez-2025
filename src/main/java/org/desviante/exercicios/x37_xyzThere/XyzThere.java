a2cdb3903fd23dcb6b7ef1126ccbb9959fbe036b
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-10-22 07:28:24 -0300

    Adicionado exercício 37 que verifica se há substring xyz precedida por um ponto

---
+++ src/main/java/org/desviante/exercicios/x37_xyzThere/XyzThere.java
@@ -0,0 +1,27 @@
+package org.desviante.exercicios.x37_xyzThere;
+
+public class XyzThere {
+    public boolean xyzThere(String str) {
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+                if (i == 0 || str.charAt(i - 1) != '.') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        XyzThere xyzThere = new XyzThere();
+        System.out.println("Case 1: " + xyzThere.xyzThere("abcxyz")); // true
+        System.out.println("Case 2: " + xyzThere.xyzThere("abc.xyz")); // false
+        System.out.println("Case 3: " + xyzThere.xyzThere("xyz.abc")); // true
+        System.out.println("Case 4: " + xyzThere.xyzThere("abcxy")); // false
+        System.out.println("Case 5: " + xyzThere.xyzThere("xyz")); // true
+        System.out.println("Case 6: " + xyzThere.xyzThere("xxyz")); // true
+        System.out.println("Case 7: " + xyzThere.xyzThere("x.xyz")); // false
+        System.out.println("Case 8: " + xyzThere.xyzThere("x.xxyz")); // true
+    }
+
+}
