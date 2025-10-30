618285523b907cbeed5626dca2a8a837c4eb5a1a
lgjor <lucasgodoyjor@gmail.com>
2025-10-30 08:22:34 -0300

    FIX: Removidas variáveis não utilizadas

--- src/main/java/org/desviante/exercicios/HyperSkill/Main.java
+++ src/main/java/org/desviante/exercicios/HyperSkill/Main.java
@@ -4,13 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int fibonacciPrevious = 1;
         int fibonacciCurrent = 1;
-        int fibonacciSum = fibonacciPrevious + fibonacciCurrent;
-
         while (true) {
             int tmp = fibonacciPrevious + fibonacciCurrent;
             fibonacciPrevious = fibonacciCurrent;
             fibonacciCurrent = tmp;
-            fibonacciSum += fibonacciCurrent;
         }
     }
 }
