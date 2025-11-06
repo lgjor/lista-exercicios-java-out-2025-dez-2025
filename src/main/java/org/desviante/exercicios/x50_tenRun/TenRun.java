50f218cb851ab0bc19727c7fe86278576281aeeb
lgjor <lucasgodoyjor@gmail.com>
2025-11-06 08:14:04 -0300

    Adicionado exercício 50

--- src/main/java/org/desviante/exercicios/x50_tenRun/TenRun.java
+++ src/main/java/org/desviante/exercicios/x50_tenRun/TenRun.java
@@ -1,4 +1,38 @@
 package org.desviante.exercicios.x50_tenRun;
 
+import java.util.Arrays;
+
 public class TenRun {
+
+    public static int[] tenRun(int[] nums)
+    {
+        if (nums==null){
+            return null;
+        }
+        if (nums.length<2){
+            return nums;
+        }
+        for (int i = 0; i<nums.length-1; i++){
+            if (nums[i]%10==0 && i<nums.length-1){
+                for (int j = i+1; i<nums.length-1; j++){
+                    if (nums[j]%10==0){
+                        break;
+                    } else {
+                        nums[j]=nums[i];
+                        i=j;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] teste01 = new int[]{2, 10, 3, 4, 20, 5};
+        int[] teste02 = new int[]{10, 1, 20, 2};
+        int[] teste03 = new int[]{10, 1, 9, 20};
+        System.out.println(Arrays.toString(tenRun(teste01)));
+        System.out.println(Arrays.toString(tenRun(teste02)));
+        System.out.println(Arrays.toString(tenRun(teste03)));
+    }
 }
--- src/test/java/org/desviante/exercicios/x50_tenRun/TenRunTest.java
+++ src/test/java/org/desviante/exercicios/x50_tenRun/TenRunTest.java
@@ -1,4 +1,54 @@
 package org.desviante.exercicios.x50_tenRun;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Tests for TenRun")
 public class TenRunTest {
+
+
+    @Test
+    @DisplayName("Quando for nulo, deve retornar nulo")
+    void nullArrayNullReturn(){
+        int[] resultado = TenRun.tenRun(null);
+        Assertions.assertNull(resultado);
+    }
+
+    @Test
+    @DisplayName("Se houver apenas 1 elemento, não faz nada")
+    void oneElementReturnOneElement(){
+        int[] nums = new int[]{10};
+        int [] resultado = TenRun.tenRun(nums);
+        Assertions.assertArrayEquals(nums, resultado);
+    }
+
+    @Test
+    @DisplayName("Se o múltiplo de 10 for o último elemento, não faz nada")
+    void lastElementTenRun(){
+        int[] nums = new int[]{1, 3, 4, 5, 10};
+        int [] resultado = TenRun.tenRun(nums);
+        Assertions.assertArrayEquals(nums, resultado);
+    }
+
+    @Test
+    @DisplayName("Deve substituir o elemento seguinte pelo múltiplo de 10 correspondente")
+    void replaceTenRunCorrectly(){
+        int[] nums = new int[]{2, 10, 3, 4, 20, 5};
+        int [] resultado = TenRun.tenRun(nums);
+        int [] expected = new int[]{2, 10, 10, 10, 20, 20};
+        Assertions.assertArrayEquals(expected, resultado);
+
+        nums = new int[]{10, 1, 20, 2};
+        resultado = TenRun.tenRun(nums);
+        expected = new int[] {10, 10, 20, 20};
+        Assertions.assertArrayEquals(expected, resultado);
+
+        nums = new int[]{10, 1, 9, 20};
+        resultado = TenRun.tenRun(nums);
+        expected = new int[] {10, 10, 10, 20};
+        Assertions.assertArrayEquals(expected, resultado);
+    }
+
 }
