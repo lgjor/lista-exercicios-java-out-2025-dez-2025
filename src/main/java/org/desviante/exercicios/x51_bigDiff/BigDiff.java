d99a6317f7557da70dc503e4de38bf4130f0f50d
lgjor <lucasgodoyjor@gmail.com>
2025-11-08 09:09:11 -0300

    Adicionado exercício 51 que identifica maior e menor elemento de um array de inteiros em java

--- src/main/java/org/desviante/exercicios/x51_bigDiff/BigDiff.java
+++ src/main/java/org/desviante/exercicios/x51_bigDiff/BigDiff.java
@@ -1,4 +1,30 @@
 package org.desviante.exercicios.x51_bigDiff;
 
+import java.security.InvalidParameterException;
+
 public class BigDiff {
+
+    public int bigDiff(int[] nums)
+    {
+        if (nums==null || nums.length<1){
+            throw new InvalidParameterException("Array teve ter mais de um elemento");
+        }
+        int valorMinimo = Integer.MAX_VALUE;
+        int valorMaximo = Integer.MIN_VALUE;
+        for (int num: nums){
+            if (num<valorMinimo){
+                valorMinimo=num;
+            }
+            if (num>valorMaximo){
+                valorMaximo=num;
+            }
+        }
+        return valorMaximo-valorMinimo;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{};
+        BigDiff bigDiff = new BigDiff();
+        System.out.println(bigDiff.bigDiff(input));
+    }
 }
