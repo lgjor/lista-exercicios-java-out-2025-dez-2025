89e1f43e1e26bfae03b094872c59a9f5ca23848d
lgjor <lucasgodoyjor@gmail.com>
2025-11-11 09:19:27 -0300

    Adicionado exercício 22 que verifica se há 2 consecutivos no array

--- src/main/java/org/desviante/exercicios/x55_has22/Has22.java
+++ src/main/java/org/desviante/exercicios/x55_has22/Has22.java
@@ -1,4 +1,15 @@
 package org.desviante.exercicios.x55_has22;
 
 public class Has22 {
+
+    public boolean has22(int[] nums)
+    {
+        for (int i=0; i<nums.length-1; i++){
+            if (nums[i]==2 & nums[i+1]==2){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
