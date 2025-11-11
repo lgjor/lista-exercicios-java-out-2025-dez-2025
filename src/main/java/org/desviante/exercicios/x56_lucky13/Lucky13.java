ca955aa87d83bf3fcfa95ee88ef3cf164a6df253
lgjor <lucasgodoyjor@gmail.com>
2025-11-11 09:25:29 -0300

    Adicionado exercício 56 que retorna false caso encontre 1 ou 3 no array de inteiros

---
+++ src/main/java/org/desviante/exercicios/x56_lucky13/Lucky13.java
@@ -0,0 +1,20 @@
+package org.desviante.exercicios.x56_lucky13;
+
+public class Lucky13 {
+
+    public boolean lucky13(int[] nums)
+    {
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]==1 || nums[i]==3){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arrayTeste = new int[]{0, 2, 4};
+        Lucky13 lucky13 = new Lucky13();
+        System.out.println(lucky13.lucky13(arrayTeste));
+    }
+}
