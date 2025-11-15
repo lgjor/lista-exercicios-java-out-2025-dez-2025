b2eaaf0b1dd04d2a0ccfd00322c6c1be46fb4a6f
lgjor <lucasgodoyjor@gmail.com>
2025-11-15 06:52:39 -0300

    Adicionado exercício 64 que verifica a existência de um 2 após um 1 no vetor de inteiros

---
+++ src/main/java/org/desviante/exercicios/x64_has12/Has12.java
@@ -0,0 +1,18 @@
+package org.desviante.exercicios.x64_has12;
+
+public class Has12 {
+
+    public boolean has12(int[] nums)
+    {
+        boolean has1 = false;
+        for (int num: nums){
+            if (num==1){
+                has1 = true;
+            }
+            else if (num==2 && has1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/desviante/exercicios/x64_has12/readme.md
@@ -0,0 +1,2 @@
+# X64: tem 12
+Dado um array de int números `s`, retorne verdadeiro `true` se houver um número 1 no array seguido por um número 2 em algum ponto posterior do array.
