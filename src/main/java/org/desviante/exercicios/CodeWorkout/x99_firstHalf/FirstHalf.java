ef1074f84fbe033400c239dc8514aafa045de9bd
lgjor <lucasgodoyjor@gmail.com>
2025-12-11 13:24:54 -0300

    Adicionado exercício 99 do CodeWorkout

--- src/main/java/org/desviante/exercicios/CodeWorkout/x99_firstHalf/FirstHalf.java
+++ src/main/java/org/desviante/exercicios/CodeWorkout/x99_firstHalf/FirstHalf.java
@@ -1,4 +1,14 @@
 package org.desviante.exercicios.CodeWorkout.x99_firstHalf;
 
 public class FirstHalf {
+
+    public String firstHalf(String str)
+    {
+        return str.substring(0,str.length()/2);
+    }
+
+    public static void main(String[] args) {
+        FirstHalf firstHalf = new FirstHalf();
+        System.out.println(firstHalf.firstHalf("WooHooha"));
+    }
 }
--- src/main/java/org/desviante/exercicios/CodeWorkout/x99_firstHalf/README.md
+++ src/main/java/org/desviante/exercicios/CodeWorkout/x99_firstHalf/README.md
@@ -5,3 +5,5 @@
 Dada uma string de comprimento par, retorne a primeira metade. Portanto, a string "WooHoo" retorna "Woo".
 
 ## Solução
+
+Retornei a metade final da string como método `str.substring(0,str.length()/2)`
